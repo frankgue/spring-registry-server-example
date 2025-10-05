6cbb42b9a886488a886cc034f76b6e54f35fad25
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-10-05 01:39:25 +0100

    first commit

---
+++ src/test/java/com/gkfcsolution/springregistryserverexample/SpringRegistryServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.springregistryserverexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegistryServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
